86bdeb05493b471dc774c3cd48d59ef4369bb048
ilder948 <ilder948@gmail.com>
2019-10-19 10:11:17 -0500

    arreglo en linea

--- resolucion.sh
+++ resolucion.sh
@@ -3,5 +3,4 @@
 gtf 1440 900 60
 xrandr --newmode "1440x900_60.00"  106.47  1440 1520 1672 1904  900 901 904 932  -HSync +Vsync
 xrandr --addmode DP-2 "1440x900_60.00"
-xrandr - salida DP-2 --modo "1440x900_60.00"
-
+xrandr --output DP-2  --mode 1440x900_60.00
